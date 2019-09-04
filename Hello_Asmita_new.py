273297b6128076f7d8526e1b7162bdd4cf8bb438
asmita chatterjee <achatterjee@coopervision.com>
2019-09-04 13:23:49 +0530

    checking thirsty

---
+++ Hello_Asmita_new.py
@@ -0,0 +1,9 @@
+print("how is the weather today?")
+import sys
+print('version is ')
+print(sys.version)
+hungry = input("are you hungry ?")
+if hungry == "yes":
+  print("eat food")
+else:
+   print("do your hw")
