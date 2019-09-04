6071235371c576dd151f461de9230c91decfd744
asmita chatterjee <achatterjee@coopervision.com>
2019-09-04 13:06:58 +0530

    some message

--- Hello_Asmita.py
+++ Hello_Asmita.py
@@ -2,3 +2,8 @@ print("how is the weather today?")
 import sys
 print('version is ')
 print(sys.version)
+hungry = input("are you hungry ?")
+if hungry == "yes":
+  print("eat food")
+else:
+   print("do your hw")
